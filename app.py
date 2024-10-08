75b1b397e27325f0518ab5426f1d01dfb76b23ba
MateoCarusotti <mateocarusotti@gmail.com>
2024-10-08 11:42:02 -0300

    agrego cambio de ingresar dato

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -8,6 +9,17 @@ users = {'admin': 'password123',
          'magma': 'ocean', 'cloud': 'flame', 
          'bird': 'leaf', 'pine': 'spoon', 
          'moon': 'storm', 'star': 'sand' }
+
+
+def buscar_string_en_archivo(nombre_archivo, string_a_buscar):
+    with open(nombre_archivo, 'r') as archivo:
+        contenido = archivo.read()  
+        
+        if string_a_buscar in contenido:  
+            return True  
+        else:
+            return False  
+
 
 @app.route('/')
 def home():
@@ -32,6 +44,18 @@ def login():
     
     return render_template('login.html')
 
+@app.route('/ingresar_dato')
+def ingresar_dato():
+    with open('scoreboard.txt' ,mode= 'a', encoding= 'UTF-8') as score:
+        username = session['username']
+        if not buscar_string_en_archivo('scoreboard.txt', username):
+        
+            current_time = datetime.now()
+
+            score.write(f"{current_time} - {username}\n")
+
+    return render_template('home.html', message= '¡Ya lo enviaste!')
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
@@ -39,3 +63,5 @@ def logout():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    with open('scoreboard.txt' ,mode= 'w', encoding= 'UTF-8') as score:
+        score.write('')
